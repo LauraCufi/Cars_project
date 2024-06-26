9476ba57b67a1eb8164cf557ac5802ef31a7d7ea
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-26 12:32:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ st.header('Car advertisement dataset')
 st.markdown("In this project, the data of a car dealer will be analyzed")
 st.sidebar.header("User input features")
 
-selected_model_year=(df.model_year)
+selected_model_year=df.model_year
 model_year=st.sidebar.selectbox('Model year',list(range(1908,2019)))
-
 
 sorted_unique_model=sorted(df.model.unique())
 selected_model=st.sidebar.multiselect("Model",sorted_unique_model,sorted_unique_model)
@@ -22,7 +21,7 @@ selected_model=st.sidebar.multiselect("Model",sorted_unique_model,sorted_unique_
 unique_condition=['excellent','fair','good','like new','new','salvage']
 selected_contidion=st.sidebar.multiselect('Condition',unique_condition,unique_condition)
 
-df_selected_model=df[(df.model.isin(selected_model)) & (df.condition.isin(selected_contidion)) & (df.model_year.isin(model_year)) ]
+df_selected_model=df[(df.model.isin(selected_model)) & (df.condition.isin(selected_contidion)) & (df.model_year.isin(model_year))]
 
 st.header("Display model and contion cars")
 st.write("Data Dimension: "+ str (df_selected_model.shape[0])+"rows and "+str(df_selected_model.shape[1]) + ' columns.')
