23f1911f23beb33593d604a4423d29b6d30e6148
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-26 15:20:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,15 @@ st.header("Display model and contion cars")
 st.write("Data Dimension: "+ str (df_selected_model.shape[0])+"rows and "+str(df_selected_model.shape[1]) + ' columns.')
 st.dataframe(df_selected_model)
 
-# -------------------------------------------------------
-#show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
-#if not show_manuf_1k_ads:
-#    df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
-#
-#st.dataframe(df)
-#st.header('Vehicle types by manufacturer')
-#st.write(px.histogram(df, x='manufacturer', color='type'))
-#st.header('Histogram of `condition` vs `model_year`')
+
+show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
+if not show_manuf_1k_ads:
+    df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
+
+st.dataframe(df)
+st.header('Vehicle types by manufacturer')
+st.write(px.histogram(df, x='manufacturer', color='type'))
+st.header('Histogram of `condition` vs `model_year`')
 # -------------------------------------------------------
 # -------------------------------------------------------
 # histograms in plotly:
@@ -48,28 +48,28 @@ st.dataframe(df_selected_model)
 # works, but too many lines of code
 # -------------------------------------------------------
 
-# histograms in plotly_express:
-#st.write(px.histogram(df, x='model_year', color='condition'))
+ histograms in plotly_express:
+st.write(px.histogram(df, x='model_year', color='condition'))
 # a lot more concise!
 # -------------------------------------------------------
 
-#st.header('Compare price distribution between manufacturers')
-#manufac_list = sorted(df['manufacturer'].unique())
-#manufacturer_1 = st.selectbox('Select manufacturer 1',
-#                              manufac_list, index=manufac_list.index('chevrolet'))
-#
-#manufacturer_2 = st.selectbox('Select manufacturer 2',
-#                              manufac_list, index=manufac_list.index('hyundai'))
-#mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
-#df_filtered = df[mask_filter]
-#normalize = st.checkbox('Normalize histogram', value=True)
-#if normalize:
-#    histnorm = 'percent'
-#else:
-#    histnorm = None
-#st.write(px.histogram(df_filtered,
-#                      x='price',
-#                      nbins=30,
-#                      color='manufacturer',
-#                      histnorm=histnorm,
-#                      barmode='overlay'))
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
+
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
