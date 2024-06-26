a2ebe8103bbdce6b7796be81eaf70b29fef2a993
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-26 12:11:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ import plotly_express as px
 import plotly.graph_objects as go
 import pyarrow as pa
 
-
+df = pd.read_csv('vehicles_us.csv')
 
 #df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
 st.header('Car advertisement dataset')
 st.markdown("In this project, the data of a car dealer will be analyzed")
 st.sidebar.header("User input features")
+
+selected_model_year=(df.model_year)
 model_year=st.sidebar.selectbox('Model year',list(range(1908,2019)))
 
-df = pd.read_csv('vehicles_us.csv')
 
 sorted_unique_model=sorted(df.model.unique())
 selected_model=st.sidebar.multiselect("Model",sorted_unique_model,sorted_unique_model)
@@ -21,7 +22,7 @@ selected_model=st.sidebar.multiselect("Model",sorted_unique_model,sorted_unique_
 unique_condition=['excellent','fair','good','like new','new','salvage']
 selected_contidion=st.sidebar.multiselect('Condition',unique_condition,unique_condition)
 
-df_selected_model=df[(df.model.isin(selected_model)) & (df.condition.isin(selected_contidion))]
+df_selected_model=df[(df.model.isin(selected_model)) & (df.condition.isin(selected_contidion)) & (df.model_year(selected_model_year)) ]
 
 st.header("Display model and contion cars")
 st.write("Data Dimension: "+ str (df_selected_model.shape[0])+"rows and "+str(df_selected_model.shape[1]) + ' columns.')
