009f14728632ca55c3bb9b645dc104c3e931bb53
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-26 11:40:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly_express as px
 import plotly.graph_objects as go
 import pyarrow as pa
 
-@st.cache
+
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
