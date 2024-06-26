0cb37445659574cde999298c8582fbcf5491d963
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-26 11:30:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,26 @@ import plotly_express as px
 import plotly.graph_objects as go
 import pyarrow as pa
 
+@st.cache
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
-
 
 st.header('Car advertisement dataset')
 st.markdown("In this project, the data of a car dealer will be analyzed")
 st.sidebar.header("User input features")
 model_year=st.sidebar.selectbox('Model year',list(range(1908,2019)))
 
+sorted_unique_model=sorted(df.model.unique())
+selected_model=st.sidebar.multiselect("Model",sorted_unique_model,sorted_unique_model)
 
+unique_condition=['excellent','fair','good','like new','new','salvage']
+selected_contidion=st.sidebar.multiselect('Condition',unique_condition,unique_condition)
 
+df_selected_model=df[(df.model(selected_model))&(df.condition.isin(selected_contidion))]
 
-
+st.header("Display model and contion cars")
+st.write("Data Dimension: "+str(df_selected_model.shape[0])+"rows and "+str(df_selected_model.shape[1]))
+st.dataframe(df_selected_model)
 
 show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
 if not show_manuf_1k_ads:
