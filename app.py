2d43a7d0f091c9b86f2577ea5fcd3a4be4d92df2
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-06-28 11:29:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
 st.header('Car advertisement dataset')
+st.markdown('Continuing, a project corresponding to Sprint 4 of TripleTen's Dat Science program was released. This project analyzes different factors from the data base of a vehicle owner and shows the conclusions of drawing graphics and analyzing data')
 st.markdown("The sidebar offers the option to filter the total data, by year, model and condition. Upon selection the data table will be updated with the selected data. Below is an analysis of the condition of the vehicle compared to the price, the types of vehicles in relation to the manufacturer and a price comparison between the selected manufacturers.")
 st.sidebar.header("User input features")
 
@@ -30,13 +31,16 @@ st.dataframe(df_selected_model)
 st.header('Condition vs price')
 st.write(px.histogram(df, x='fuel', color='condition'))
 
-st.markdown('The bar chart compares the condition of the vehicle, which can be: excellent, new, like new, good, acceptable and salvage, with the price. We can conclude that gas vehicles, which have the greatest offer in general, also have the greatest offer in terms of vehicle conditions, counting all of them, with the highest price being in "excellent" condition. On the other hand, electric fuel vehicles do not have such a variety, in fact, they do not have options available in "acceptable" quality and in the same way the price is not that high.')
+st.markdown("The bar chart compares the condition of the vehicle, which can be: excellent, new, like new, good, acceptable and salvage, with the price. We can conclude that gas vehicles, which have the greatest offer in general, also have the greatest offer in terms of vehicle conditions, counting all of them, with the highest price being in "excellent" condition. On the other hand, electric fuel vehicles do not have such a variety, in fact, they do not have options available in "acceptable" quality and in the same way the price is not that high")
 
 
 st.header('Vehicle types by manufacturer')
 st.write(px.histogram(df, x='manufacturer', color='type'))
 
+st.markdown("In the vehicle type graph vs the manufacturer, compare the different manufacturers offered and the types of vehicles available between those that are shown: suv, sedam, cup, cupe, among others.
 
+From the graphics we can conclude that the manufacturer has the majority of offers available in a variety of vehicle types such as Ford.
+The manufacturer with a small variety of Mercedes-Benz vehicles only offers “van” type vehicles.")
 
 st.header('Compare price distribution between manufacturers')
 manufac_list = sorted(df['manufacturer'].unique())
