97c3df0172aa93ca1c284a07905ae5e3c066d5eb
LauraCufi <101473539+LauraCufi@users.noreply.github.com>
2024-07-01 11:03:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ st.dataframe(df_selected_model)
 st.header("Type vs price")
 #st.pyplot(df.groupby('type')['price'].mean().plot(kind='bar'))
 mean_prices = df.groupby('type')['price'].mean().reset_index()
-fig = px.bar(mean_prices, x='type', y='price')
-fig.show()
+st.write(px.bar(mean_prices, x='type', y='price'))
+
 
 
 
